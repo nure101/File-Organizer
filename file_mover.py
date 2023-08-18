d99b7a90b257e758fa5656113e9ee8368f7f2678
nure101 <58121428+nure101@users.noreply.github.com>
2023-08-18 12:36:21 -0500

    Update file_mover.py

--- file_mover.py
+++ file_mover.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
 
-root = (os.scandir('/Users/nuremo/Desktop'))
+root = (os.scandir('/Your/dir/path'))
 
 for i in root:
     if 'Screenshot' in str(i):
         #print (i.name)
         if i.is_file():
-            os.chdir('/Users/nuremo/Desktop')
+            os.chdir('/Your/dir/path')
             os.system(f" mv '{i.path}' screenshots")
 
         #os.scandir('/Users/nuremo/Desktop')
