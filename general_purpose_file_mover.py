63f02db29f40b4051302d3f446eb3ef6c3e26b44
nure101 <58121428+nure101@users.noreply.github.com>
2023-08-18 12:36:54 -0500

    Update general_purpose_file_mover.py

--- general_purpose_file_mover.py
+++ general_purpose_file_mover.py
@@ -2,14 +2,13 @@ import os
 from pathlib import Path
 
 user_root = input("Complete path you want to access: ")
-print (user_root)
 move_from = input("Move all: ")
 move_to = input("To: ")
 root = os.scandir(user_root) # where the files are located
 
 # loop through everything in that directory 
 for i in root:
-    # don't forget capitalization  
+    # Don't forget capitalization  
     if move_from in str(i) and i.is_file():
         print (i.path)
         os.chdir(user_root)
